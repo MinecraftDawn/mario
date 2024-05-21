b0cbadc50adb71e2f24205645b46ae966eda04a5
DerrickPikachu <derrfgh4563@gmail.com>
2024-05-21 09:23:51 +0800

    Add: goal item, launch a GameFinish event, but there is no concrete implementation of the effect yet.

---
+++ Assets/Scripts/Event/GameFinish.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Event
+{
+
+public class GameFinish : EventBase
+{
+    public void UpdateContext(GameContext context)
+    {
+        Debug.Log("game finished");
+    }
+}
+
+}
