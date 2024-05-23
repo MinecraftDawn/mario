46a1b5a7cbcfc31ffa13018d69e73f890e0aa534
MinecraftDawn <eric4902077@gmail.com>
2024-05-23 15:58:12 +0800

    refactor: move playerDetectTime from Monster to TracePlayer

--- Assets/Scripts/Strategy/TracePlayer.cs
+++ Assets/Scripts/Strategy/TracePlayer.cs
@@ -9,24 +9,42 @@ namespace Strategy
 public class TracePlayer : KeepMove
 {
     public float speedUpWhenTracing = 1.5f;
+    public float playerDetectTime = 3f;
     public float tracePlayerTurnAroundDelay = 0.8f;
-    
+    public Detector playerDetector;
+    private GameObject _player;
+    private float _lastPlayerDetectTime;
     private float _lastTracePlayerTurnAroundTime;
 
     public override void Decide(Monster monster)
     {
-        if (!monster.IsPlayerFound()) {
-            base.Decide(monster);
-        } else {
-            // trace player
+        UpdatePlayerDetection();
+        if (IsPlayerDetected()) {
             Tracing(monster);
         }
+        else {
+            base.Decide(monster);
+        }
+    }
+    
+    private void UpdatePlayerDetection() {
+        if (playerDetector.IsDetected()) {
+            _player = playerDetector.GetDetectedObject();
+            UpdateLastDetectPlayerTime();
+        }
+
+        if (IsForgetPlayer()) { _player = null; }
+    }
+    
+    private bool IsPlayerDetected()
+    {
+        return _player != null;
     }
 
     private void Tracing(Monster monster)
     {
         Vector2 monster_to_player = 
-            monster.GetPlayer().transform.position - monster.transform.position;
+            _player.transform.position - monster.transform.position;
         if ((monster.GetMoveToRight() && monster_to_player.x < 0f) ||
             (!monster.GetMoveToRight() && monster_to_player.x > 0f)) {
             if (CanTurnAroundToTrackPlayer()) {
@@ -46,6 +64,16 @@ public class TracePlayer : KeepMove
     private void UpdateLastTurnAroundTime()
     {
         _lastTracePlayerTurnAroundTime = Time.time;
+    }
+    
+    private bool IsForgetPlayer()
+    {
+        return Time.time - _lastPlayerDetectTime > playerDetectTime;
+    }
+    
+    private void UpdateLastDetectPlayerTime()
+    {
+        _lastPlayerDetectTime = Time.time;
     }
 }
 
